c1cf3dc98eba811c01de726ee0c4951dce92c93f
HarryStemwork <162728191+HarryStemwork@users.noreply.github.com>
2024-07-16 11:25:06 +0800

    Create lesson11.py

---
+++ lesson11.py
@@ -0,0 +1,14 @@
+def arithmetic(num1, num2):
+  a = num1 + num2
+  b = num1 - num2
+  c = num1 * num2
+  d = num1 / num2
+  return a, b, c, d
+
+
+a, b, c, d = arithmetic(10, 2)
+
+print("Addition: ", )
+print("Subtraction: ", )
+print("Multiplication: ", )
+print("Division: ", )
