243036e365d2f99f1c47a8a8691ea89f18c49a77
HarryStemwork <162728191+HarryStemwork@users.noreply.github.com>
2024-07-16 11:05:09 +0800

    Create lesson0.py

---
+++ lesson0.py
@@ -0,0 +1,9 @@
+import math
+
+r = 5
+print("The area of your circle are:")
+area = math.pi ** 2 * r
+print(area)
+print("The circumference of your circle are:")
+circumference = math.pi * 2 * r
+print(circumference)
