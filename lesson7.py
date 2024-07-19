80cc6bd7027b5179a232f8116d6783b4f8729786
HarryStemwork <162728191+HarryStemwork@users.noreply.github.com>
2024-07-19 10:08:19 +0800

    Update lesson7.py

--- lesson7.py
+++ lesson7.py
@@ -1,1 +1,13 @@
-
+store = "Harry Shopping Mall"
+product = ["TV", "frigde", "fan", "cooler", "computer"] #create 5 products you will see in a shopping mall, remember to use string for all
+cart = []
+print(store)
+print(product, '\n')
+while True:
+  msg = input("Please enter what you want to buy (q=Quit) : ")
+  if msg == 'q' or msg == 'Q':
+    break
+  else:
+    if msg in product:
+      cart.append(msg)
+print("You have bought :", cart)
